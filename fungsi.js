938ddcb05802f34368d8057df61ab15f605fdd66
Damret <dsaputra2500@gmail.com>
2021-07-29 08:12:19 +0700

    commit

---
+++ fungsi.js
@@ -0,0 +1,7 @@
+function imgSlider(ganti){
+    document.querySelector('.botol').src = ganti;
+}
+function gantiWarna(warna){
+    const sec = document.querySelector('.sec');
+    sec.style.background = warna;
+}
